505365b99069516a380b1b525b08a3404beb8fb2
Artem <artem20work@gmail.com>
2023-08-07 16:45:45 +0300

    Finished hovework two

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,14 +1,14 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  const calculation = Math.pow(b**2-4*a*c)
+  const calculation = Math.pow(b**2) - 4*a*c
 
   if (calculation < 0) {
     arr = [];
   } else if (calculation == 0) {
-    const root = -b/(2*a)
+    const root = -b/(2*a);
     arr = [root];
-  } else if (calculation > 0) {
+  } else {
     const rootOne = (-b + Math.sqrt(d) )/(2*a);
     const rootTwo = (-b - Math.sqrt(d) )/(2*a);
     arr = [rootOne, rootTwo];
@@ -18,6 +18,21 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  if (
+    typeof percent !== "number" ||
+    typeof contribution !== "number" ||
+    typeof amount !== "number" ||
+    typeof countMonths !== "number" ||
+    percent < 0 ||
+    percent > 100 ||
+    contribution < 0 ||
+    amount <= 0 ||
+    countMonths <= 0 ||
+    !Number.isInteger(countMonths) // Проверка на дроби
+  ) {
+    return alert("Ошибка входных данных. Убедитесь, что все аргументы являются числами и введены корректные значения");
+  } // Проверка валидности
+  
     const monthPercent = percent / 100 / 12; // Месячный процент
 
     const refundAmount = amount - contribution; // Тело кредита, сумма для возврата банку.
